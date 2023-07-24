413ce9fec7383458f923486cc3556da1e751597d
Mateo L <mateolopezw@hotmail.com>
2023-07-24 18:23:56 -0500

    lista de tareas node promesas

---
+++ README.md
@@ -0,0 +1,15 @@
+¡Claro! Aquí tienes una versión más resumida y en un lenguaje más coloquial:
+
+### Uso de async y await
+
+Cuando usamos `async` y `await`, nuestro código para hacer cosas que llevan tiempo (como cargar datos de internet) se vuelve más fácil de entender. Podemos esperar a que esas cosas se terminen sin bloquear todo lo demás. Es como si nuestro código se tomara su tiempo sin detenerse por completo.
+
+### Uso del método then()
+
+El método `then()` es otra forma de manejar cosas que llevan tiempo. Es como tener una lista de cosas que queremos hacer en orden. Primero hacemos una cosa, y cuando se termina, pasamos a la siguiente. Así hasta que hayamos hecho todo lo que necesitamos.
+
+### Diferencias entre async, await y el método then()
+
+La principal diferencia es cómo se ve el código. Con `async` y `await`, nuestro código parece más normal, como una receta paso a paso. Es fácil de leer y entender. Con `then()`, nuestro código se parece más a una serie de cajas dentro de otras cajas. Puede ser más difícil de leer, especialmente cuando hay muchas cosas que hacer.
+
+En resumen, `async/await` nos ayuda a escribir código más claro y fácil de leer, mientras que el método `then()` puede hacer que nuestro código se vea más enredado. Ambos son útiles para lidiar con tareas que toman tiempo, pero `async/await` es preferido cuando queremos que nuestro código sea más sencillo y fácil de mantener.
--- todo.js
+++ todo.js
@@ -1,85 +1,120 @@
 const readline = require("readline");
 
+const tasks = [];
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const tasks = [];
-
-function addTask(indicator, description) {
-  tasks.push({
-    indicator,
-    description,
-    completed: false,
+function addTask() {
+  return new Promise((resolve) => {
+    rl.question("Descripción de la tarea: ", (description) => {
+      tasks.push({
+        indicator: tasks.length + 1,
+        description,
+        completed: false,
+      });
+      console.log("Tarea agregada con éxito.");
+      resolve();
+    });
   });
 }
 
-function removeTask(indicator) {
-  const taskIndex = tasks.findIndex((task) => task.indicator === indicator);
-  if (taskIndex !== -1) {
-    tasks.splice(taskIndex, 1);
-  }
+function removeTask() {
+  return new Promise((resolve, reject) => {
+    rl.question(
+      "Ingrese el indicador de la tarea que desea eliminar: ",
+      (indicator) => {
+        const index = indicator - 1;
+        if (tasks[index]) {
+          tasks.splice(index, 1);
+          console.log("Tarea eliminada con éxito.");
+          resolve();
+        } else {
+          console.log("Indicador inválido. Intente nuevamente.");
+          reject();
+        }
+      }
+    );
+  });
 }
 
-function completeTask(indicator) {
-  const task = tasks.find((task) => task.indicator === indicator);
-  if (task) {
-    task.completed = true;
-  }
+function completeTask() {
+  return new Promise((resolve, reject) => {
+    rl.question(
+      "Ingrese el indicador de la tarea completada: ",
+      (indicator) => {
+        const index = indicator - 1;
+        if (tasks[index]) {
+          tasks[index].completed = true;
+          console.log("Tarea completada con éxito.");
+          resolve();
+        } else {
+          console.log("Indicador inválido. Intente nuevamente.");
+          reject();
+        }
+      }
+    );
+  });
 }
 
 function displayTasks() {
-  console.log("--- Tasks ---");
+  console.log("\nLista de tareas:");
   tasks.forEach((task) => {
-    const status = task.completed ? "Completed" : "Not completed";
-    console.log(`${task.indicator}: ${task.description} (${status})`);
+    const status = task.completed ? "Completada" : "Pendiente";
+    console.log(`${task.indicator}. ${task.description} - ${status}`);
   });
-  console.log("-------------");
+  console.log("\n");
+  rl.prompt();
 }
 
-function promptUser() {
-  rl.question(
-    "Enter a command (add, remove, complete, list, quit): ",
-    (command) => {
-      switch (command) {
-        case "add":
-          rl.question("Enter task indicator: ", (indicator) => {
-            rl.question("Enter task description: ", (description) => {
-              addTask(indicator, description);
-              displayTasks();
-              promptUser();
-            });
-          });
-          break;
-        case "remove":
-          rl.question("Enter task indicator to remove: ", (indicator) => {
-            removeTask(indicator);
-            displayTasks();
-            promptUser();
-          });
-          break;
-        case "complete":
-          rl.question("Enter task indicator to complete: ", (indicator) => {
-            completeTask(indicator);
-            displayTasks();
-            promptUser();
-          });
-          break;
-        case "list":
+function showMenu() {
+  console.log("----- Menú -----");
+  console.log("1. Agregar tarea");
+  console.log("2. Eliminar tarea");
+  console.log("3. Marcar tarea como completada");
+  console.log("4. Salir");
+  console.log("-----------------");
+}
+
+async function main() {
+  showMenu();
+
+  rl.on("line", async (input) => {
+    switch (input.trim()) {
+      case "1":
+        await addTask();
+        displayTasks();
+        break;
+      case "2":
+        try {
+          await removeTask();
           displayTasks();
-          promptUser();
-          break;
-        case "quit":
-          rl.close();
-          break;
-        default:
-          console.log("Invalid command. Try again.");
-          promptUser();
-          break;
-      }
+        } catch (error) {
+          displayTasks();
+        }
+        break;
+      case "3":
+        try {
+          await completeTask();
+          displayTasks();
+        } catch (error) {
+          displayTasks();
+        }
+        break;
+      case "4":
+        rl.close();
+        break;
+      default:
+        console.log("Opción inválida. Intente nuevamente.");
+        break;
     }
-  );
+  });
+
+  await new Promise((resolve) => rl.on("close", resolve));
+  console.log("¡Hasta luego!");
+  process.exit(0);
 }
 
-promptUser();
+main();
